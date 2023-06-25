b2e2e0d7149187a34944cc3e00ed433da437cf5e
pavelsmirnov77 <pavelsmirnov35rus@yandex.ru>
2023-06-25 05:00:47 +0300

    Исправлен скрипт прокрутки карусели

--- 5-spring-boot-and-mvc/src/main/resources/static/scripts/carousel.js
+++ 5-spring-boot-and-mvc/src/main/resources/static/scripts/carousel.js
@@ -1,33 +1,23 @@
-// carousel.js
 const carousel = document.querySelector('.carousel');
 const carouselInner = carousel.querySelector('.carousel-inner');
 const carouselItems = Array.from(carouselInner.children);
 const prevButton = carousel.querySelector('.carousel-prev');
 const nextButton = carousel.querySelector('.carousel-next');
 
-// Количество видов пива в карусели
-const itemCount = carouselItems.length;
-
-// Ширина каждого элемента (вид пива)
-const itemWidth = carouselItems[0].offsetWidth;
-
-// Позиция текущего элемента
-let currentPosition = 0;
-
-// Обработчик нажатия на кнопку "Предыдущий"
 prevButton.addEventListener('click', () => {
-    currentPosition = (currentPosition - 1 + itemCount) % itemCount;
-    updateCarouselPosition();
+    const currentItem = carousel.querySelector('.carousel-item.active');
+    const prevItem = currentItem.previousElementSibling;
+    if (prevItem) {
+        currentItem.classList.remove('active');
+        prevItem.classList.add('active');
+    }
 });
 
-// Обработчик нажатия на кнопку "Следующий"
 nextButton.addEventListener('click', () => {
-    currentPosition = (currentPosition + 1) % itemCount;
-    updateCarouselPosition();
+    const currentItem = carousel.querySelector('.carousel-item.active');
+    const nextItem = currentItem.nextElementSibling;
+    if (nextItem) {
+        currentItem.classList.remove('active');
+        nextItem.classList.add('active');
+    }
 });
-
-// Обновление позиции карусели
-function updateCarouselPosition() {
-    const translateX = -currentPosition * itemWidth;
-    carouselInner.style.transform = `translateX(${translateX}px)`;
-}
