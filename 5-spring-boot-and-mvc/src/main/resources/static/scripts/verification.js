bd1a78e614c9ed000a95b99d3f602c0d53becc13
pavelsmirnov77 <pavelsmirnov35rus@yandex.ru>
2023-06-25 22:32:14 +0300

    Исправлена блокировка скролла до подтверждения возраста

--- 5-spring-boot-and-mvc/src/main/resources/static/scripts/verification.js
+++ 5-spring-boot-and-mvc/src/main/resources/static/scripts/verification.js
@@ -4,5 +4,16 @@ window.addEventListener('DOMContentLoaded', function() {
 
     ageConfirmButton.addEventListener('click', function() {
         ageVerificationOverlay.style.display = 'none';
+        document.body.style.overflow = 'auto'; // Разрешить прокрутку страницы после подтверждения
     });
+
+    ageVerificationOverlay.addEventListener('wheel', function(event) {
+        event.preventDefault(); // Предотвращаем прокрутку при прокрутке колесом мыши над оверлеем
+    });
+
+    ageVerificationOverlay.addEventListener('touchmove', function(event) {
+        event.preventDefault(); // Предотвращаем прокрутку при касании и прокрутке на оверлее на сенсорных устройствах
+    });
+
+    document.body.style.overflow = 'hidden'; // Запретить прокрутку страницы при отображении оверлея
 });
