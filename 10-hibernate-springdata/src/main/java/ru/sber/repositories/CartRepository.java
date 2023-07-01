3967c67597caefee45c5fb0487cadf636ed2846a
Pavel Smirnov <64857615+pavelsmirnov77@users.noreply.github.com>
2023-07-01 14:57:22 +0300

    Удаление директории с домашним заданием №10

--- 10-hibernate-springdata/src/main/java/ru/sber/repositories/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.sber.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.sber.entities.Cart;
-
-@Repository
-public interface CartRepository extends JpaRepository<Cart, Long> {
-}
