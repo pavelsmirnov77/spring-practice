a18b0529cfc9b27226e82b8da596315b9156ed71
pavelsmirnov77 <pavelsmirnov35rus@yandex.ru>
2023-07-07 13:28:28 +0300

    Форматирование кода

--- 14-spring-security/backend/src/main/java/ru/sber/entities/User.java
+++ 14-spring-security/backend/src/main/java/ru/sber/entities/User.java
@@ -11,7 +11,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table( name = "users",
+@Table(name = "users",
         uniqueConstraints = {
                 @UniqueConstraint(columnNames = "username"),
                 @UniqueConstraint(columnNames = "email")
